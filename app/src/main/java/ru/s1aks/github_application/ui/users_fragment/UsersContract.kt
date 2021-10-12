1306e5008ebe44cb2d3fda2510b1fcbf518d65ea
Viacheslav Khrykin <vh78aks@gmail.com>
2021-10-12 22:33:53 +0300

    Homework 03: add rxjava in project

--- app/src/main/java/ru/s1aks/github_application/ui/users_fragment/UsersContract.kt
+++ app/src/main/java/ru/s1aks/github_application/ui/users_fragment/UsersContract.kt
@@ -3,8 +3,6 @@ package ru.s1aks.github_application.ui.users_fragment
 import moxy.MvpPresenter
 import moxy.MvpView
 import moxy.viewstate.strategy.alias.AddToEndSingle
-import ru.s1aks.github_application.domain.entities.GithubUser
-import ru.s1aks.github_application.ui.BackButtonListener
 
 class UsersContract {
 
@@ -12,11 +10,11 @@ class UsersContract {
     interface View : MvpView {
         fun init()
         fun updateList()
+        fun showError(message: String)
     }
 
     abstract class Presenter : MvpPresenter<View>() {
         abstract fun loadData()
-        abstract fun loadUser(position: Int): GithubUser
         abstract fun backPressed(): Boolean
     }
 }
