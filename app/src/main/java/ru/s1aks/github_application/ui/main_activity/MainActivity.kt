adc1aa4b33e7cd897cc989eb0ee964a2b12c3546
Viacheslav Khrykin <vh78aks@gmail.com>
2021-11-07 15:16:51 +0300

    Homework 07: Add Koin DI in project

--- app/src/main/java/ru/s1aks/github_application/ui/main_activity/MainActivity.kt
+++ app/src/main/java/ru/s1aks/github_application/ui/main_activity/MainActivity.kt
@@ -1,17 +1,21 @@
 package ru.s1aks.github_application.ui.main_activity
 
 import android.os.Bundle
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
+import org.koin.android.ext.android.inject
 import ru.s1aks.github_application.databinding.ActivityMainBinding
-import ru.s1aks.github_application.impl.util.app
 import ru.s1aks.github_application.ui.BackButtonListener
 
 class MainActivity : MvpAppCompatActivity(), MainContract.View {
 
     private lateinit var binding: ActivityMainBinding
-    private val presenter by moxyPresenter { MainPresenter(app.router) }
+    private val router: Router by inject()
+    private val navigatorHolder: NavigatorHolder by inject()
+    private val presenter by moxyPresenter { MainPresenter(router) }
     private val navigator by lazy { AppNavigator(this, binding.root.id) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +26,12 @@ class MainActivity : MvpAppCompatActivity(), MainContract.View {
 
     override fun onResumeFragments() {
         super.onResumeFragments()
-        app.navigatorHolder.setNavigator(navigator)
+        navigatorHolder.setNavigator(navigator)
     }
 
     override fun onPause() {
         super.onPause()
-        app.navigatorHolder.removeNavigator()
+        navigatorHolder.removeNavigator()
     }
 
     override fun onBackPressed() {
